e346677a225da5141b057239a620b27ebf09696f
Gabe <USER@Gabes-MacBook-Air.local>
2016-06-03 10:09:37 -0700

    update hello py

--- hello.py
+++ hello.py
@@ -16,9 +16,16 @@ app = Flask(__name__)
 kvs = {}
 ip = os.environ['IP']
 port = os.environ['PORT']
+members = os.environ['MEMBERS']
 
+#Initialize the first list of nodes =! primary node
+membersk=str(members)
+memberslist=membersk.split(',')
 #This is the default master/primary node
-masternode=0
+masternode='5'
+
+memberslistIds =[x[8:9] for x in memberslist]
+
 
 
 @app.route('/myip')
@@ -48,17 +55,47 @@ def echobot():
 			return msg
 
 
+
+
 @app.route('/test')
 def node():
 	node = whatport()
 	ipstr = str(node)
 	ipsplit = ipstr.split(':')
 	ipnum = ipsplit[0]
-	ipnode = ipnum[8:9]
-	return ipnode
+	if ipnum == 'localhost':
+		return 0
+	else:
+		return 1
+	#ipnode = ipnum[4:5]
+	#return ipnode
+
+def nodek():
+	node = whatport()
+	ipstr = str(node)
+	ipsplit = ipstr.split(':')
+	ipnum = ipsplit[1]
+	ipnode = ipnum[4:5]
+	return ipnode	
+
+def isnode():
+	node = whatport()
+	ipstr = str(node)
+	ipsplit = ipstr.split(':')
+	ipnum = ipsplit[0]
+	if ipnum[7:8] == '2':
+		return True 
+	else:
+		return False
 
 	#if (node=="10.0.0.1:49160"):
 	#	return 'found'
+
+@app.route('/testx')
+def testx():
+	x=whatport()
+	return x
+	#requests.get('http://localhost:49161/myport')
 
 @app.route('/kvs/<key>', methods = ['PUT', 'GET', 'DELETE'])
 def initKVS(key):
@@ -66,29 +103,53 @@ def initKVS(key):
 	# x = VALUE
 	x = request.form.get('val')
 
-	thisnode = node()
-	if thisnode==masternode:
-		#Do PUT
-		if request.method == 'PUT':
-			theResponse = kvsput(key, x)
-			return theResponse
 
-		#Do DELETE
-		if request.method == 'DELETE':
-			theResponse = kvsdel(key, x)
-			return theResponse
+	checknode = node()
+	whichnode=nodek()
 
-		#Do GET		
+	#check if the request came from 
+	#a node and not a user
+	if checknode ==1:
+		#check if this node is the master mode	
+		if whichnode==masternode:
+			#Do PUT
+			if request.method == 'PUT':
+				theResponse = kvsput(key, x)
+				return theResponse
+
+			#Do DELETE
+			if request.method == 'DELETE':
+				theResponse = kvsdel(key, x)
+				return theResponse
+
+			#Do GET		
+			else:
+				theResponse = kvsget(key, x)
+				return theResponse
 		else:
-			theResponse = kvsget(key, x)
-			return theResponse
+			if request.method == 'PUT':
+				theResponse = kvsput(key, x)
+				return theResponse
+
+			#Do DELETE
+			if request.method == 'DELETE':
+				theResponse = kvsdel(key, x)
+				return theResponse
+
+			#Do GET		
+			else:
+				theResponse = kvsget(key, x)
+				return theResponse
 	else:
-		#Do PUT
 		if request.method == 'PUT':
-			theResponse = kvsput(key, x)
-			return theResponse
+				mainput = kvsput(key, x)
+				theResponse = nodekvsput(key, x)
+				if theResponse != 'Success':
+					return mainput
+				else:
+					return theResponse
 
-		#Do DELETE
+			#Do DELETE
 		if request.method == 'DELETE':
 			theResponse = kvsdel(key, x)
 			return theResponse
@@ -97,6 +158,21 @@ def initKVS(key):
 		else:
 			theResponse = kvsget(key, x)
 			return theResponse
+	
+
+
+	
+def nodekvsput(key, x):
+	f = str(key)
+	r = requests.put('http://10.0.0.20:12345/kvs/'+f , data = {'val':x})
+	if r.status_code !=(200 or 201):
+		return r.status_code
+	else:
+		return 'Success'
+
+#def broadcastput(key,x):
+#	for x in memberslistIds:
+
 
 
 def kvsget(key,x):
@@ -141,13 +217,12 @@ def kvsdel(key,x):
 def kvsput(key, x):
 	if kvs.get(key) == None:
 			kvs[key] = x
-			r = requests.get('http://10.0.0.20:12345/kvs/foo')
+			#r = requests.get('http://10.0.0.22:12347/testx')
 			data = {
 			'replaced' : 0,
-			'msg' : 'success',
-			'status': r.status_code
+			'msg' : 'success'
 			}
-			payload = {'val': x}
+			#payload = {'val': x}
 			response = jsonify(data)
 			response.status_code = 201
 			return response
